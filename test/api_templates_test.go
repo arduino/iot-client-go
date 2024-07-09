22fa8f54ee234375d123a470ee4c0c2d2602c77a
Marco Colombo <ing.marco.colombo@gmail.com>
2024-07-09 14:28:16 +0200

    Updated module version

--- test/api_templates_test.go
+++ test/api_templates_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
-	openapiclient "github.com/arduino/iot-client-go"
+	openapiclient "github.com/arduino/iot-client-go/v2"
 )
 
 func Test_iot_TemplatesApiService(t *testing.T) {
